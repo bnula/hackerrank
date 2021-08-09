7adee3dbb463e22c2971097289e3d28ae39c1b0d
bnula <bnulagnula@gmail.com>
2021-08-10 00:04:05 +0200

    arrayHourglas

---
+++ ArrayHourglass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class ArrayHourglass
+    {
+        public static int HighestSum(List<List<int>> arr)
+        {
+            int max = int.MinValue;
+            var sum = 0;
+            int row;
+            var colStop = 2;
+            var colStart = 0;
+            var hglevel = 0;
+            var hg = 0;
+            while (true)
+            {
+                row = hg + hglevel;
+                if (hglevel == 1)
+                {
+                    colStart++;
+                    colStop--;
+                }
+                for (int col = colStart; col <= colStop; col++)
+                {
+                    sum += arr[row][col];
+                    //Console.WriteLine(arr[row][col]);
+                }
+                if (hglevel == 1)
+                {
+                    colStart--;
+                    colStop++;
+                }
+                else if (hg == 3 && hglevel == 2 && colStop == 5)
+                {
+                    Console.WriteLine($"sum - {sum}");
+                    if (sum > max) max = sum;
+                    return max;
+                }
+                else if (hg == 3 && hglevel == 2)
+                {
+                    Console.WriteLine($"sum - {sum}");
+                    Console.WriteLine("=========");
+                    hg = 0;
+                    hglevel = -1;
+                    colStart++;
+                    colStop++;
+                    if (sum > max) max = sum;
+                    sum = 0;
+                }
+                else if (hglevel == 2)
+                {
+                    Console.WriteLine($"sum - {sum}");
+                    Console.WriteLine("=========");
+                    hg++;
+                    hglevel = -1;
+                    if (sum > max) max = sum;
+                    sum = 0;
+                }
+                hglevel++;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,9 +7,17 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            var str = "aba";
-            var num = RepeatedString.NumOfAs(str, 10);
-            Console.WriteLine(num);
+            List<List<int>> arr = new List<List<int>> 
+            {
+                new List<int> {3, 7, -3, 0, 1, 8},
+                new List<int> {1, -4, -7, -8, -6, 5},
+                new List<int> {-8, 1, 3, 3, 5, 7},
+                new List<int> {-2, 4, 3, 1, 2, 7},
+                new List<int> {2, 4, -5, 1, 8, 4},
+                new List<int> {5, -7, 6, 5, 2, 8}
+            };
+            var max = ArrayHourglass.HighestSum(arr);
+            Console.WriteLine(max);
         }
     }
 }
