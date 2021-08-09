dfdb5ec620087a3afeade6c1bb5a913f69be639d
bnula <bnulagnula@gmail.com>
2021-08-09 21:39:20 +0200

    jumpingClouds v1

---
+++ JumpingClouds.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class JumpingClouds
+    {
+        public static int NumOfJumps(List<int> c)
+        {
+            var end = c.Count();
+            bool skip = false;
+            int jumps = 0;
+            for (int i = 0; i < end-1; i++)
+            {
+                if (skip)
+                {
+                    skip = false;
+                    continue;
+                }
+                if (i != end - 2)
+                {
+                    if (c[i + 2] == 0)
+                    {
+                        jumps++;
+                        skip = true;
+                        continue;
+                    }
+                }
+                if (c[i+1] == 0)
+                {
+                    jumps++;
+                    continue;
+                }
+            }
+            return jumps;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            var path = "DDUUDDUDUUUD";
-            var valleys = CountingValleys.cntValleys(8, path);
-            Console.WriteLine(valleys);
+            var c = new List<int> { 0, 0, 0, 1, 0, 0 };
+            var jumps = JumpingClouds.NumOfJumps(c);
+            Console.WriteLine(jumps);
         }
     }
 }
