66db7b1b41d3eb261d474989e60f122657ab42e9
bnula <bnulagnula@gmail.com>
2021-08-10 15:59:51 +0200

    list leftRotation + list swap

---
+++ ArrayRotation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class ArrayRotation
+    {
+        public static List<int> RotateLeft(List<int> a, int d)
+        {
+            Queue<int> inputs = new Queue<int>(a);
+            var resultList = new List<int>();
+
+            while (d-- > 0)
+            {
+                inputs.Enqueue(inputs.Dequeue());
+            }
+
+            return inputs.ToList();
+        }
+
+        public static List<int> ReverseArray(List<int> a, int d)
+        {
+            Stack<int> inputs = new Stack<int>(a);
+            var resultList = new List<int>();
+
+            while (d-- > 0)
+            {
+                inputs.Push(inputs.Pop());
+            }
+
+            foreach (int input in inputs)
+            {
+                resultList.Add(input);
+            }
+            return resultList;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,17 +7,12 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            List<List<int>> arr = new List<List<int>> 
+            List<int> arr = new List<int> { 1, 2, 3, 4, 5 };
+            var l = ArrayRotation.ReverseArray(arr, 1);
+            foreach (var item in l)
             {
-                new List<int> {3, 7, -3, 0, 1, 8},
-                new List<int> {1, -4, -7, -8, -6, 5},
-                new List<int> {-8, 1, 3, 3, 5, 7},
-                new List<int> {-2, 4, 3, 1, 2, 7},
-                new List<int> {2, 4, -5, 1, 8, 4},
-                new List<int> {5, -7, 6, 5, 2, 8}
-            };
-            var max = ArrayHourglass.HighestSum(arr);
-            Console.WriteLine(max);
+                Console.Write($"{item} ");
+            }
         }
     }
 }
